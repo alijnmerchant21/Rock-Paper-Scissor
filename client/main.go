dac585e1ef1d9da87131fa52790365b6c9848773
Aliasgar Merchant <aliasgarmerchant@Aliasgars-MBP.fios-router.home>
2021-08-28 18:34:29 -0400

    Refactor code

--- client/main.go
+++ client/main.go
@@ -7,7 +7,6 @@ import (
 	//"strconv"
 )
 
-
 func main() {
 
 	rand.Seed(time.Now().UnixNano())
@@ -18,55 +17,95 @@ func main() {
 	var user1 string
 	var ch1 int
 
+	fmt.Println("Welcome to Rock Paper Scissor")
 
-	fmt.Println("Welcome to Rock Paper Scissor \n")
-	
-	fmt.Println("User enter your name. ")
-	fmt.Scanln(&user1)	
+	fmt.Println("\n User enter your name. ")
+	fmt.Scanln(&user1)
 	//fmt.Println("User 2 enter your name. ")
 	//fmt.Scanln(&user2)
 
 	fmt.Println("\n" + user1 + " v/s " + "Computer")
-	
+
 	fmt.Println("\n" + user1 + " Enter your choice - 1. Rock; 2. Paper; 3. Scissor")
 	fmt.Scanln(&ch1)
 
 	//fmt.Println("\n" + user2 + " Enter your choice - 1. Rock; 2. Paper; 3. Scissor")
 	//fmt.Scanln(&ch2)
 
-	if(ch1 == 1 && cp == 1) {
+	switch ch1 {
+
+	case 1:
+		fmt.Println("\n" + user1 + " your choice is Rock")
+		if cp == 1 {
+			fmt.Println("\n Game is a draw")
+		}
+		if cp == 2 {
+			fmt.Println("\n Computer is a winner")
+		}
+		if cp == 3 {
+			fmt.Println("\n" + user1 + " is a winner")
+		}
+
+	case 2:
+		fmt.Println("\n" + user1 + " your choice is Paper")
+		if cp == 1 {
+			fmt.Println("\n" + user1 + " is a winner")
+		}
+		if cp == 2 {
+			fmt.Println("\n Game is a draw")
+		}
+		if cp == 3 {
+			fmt.Println("\n Computer is a winner")
+		}
+
+	case 3:
+		fmt.Println("\n" + user1 + " your choice is Scissor")
+		if cp == 1 {
+			fmt.Println("\n Computer is a winner")
+		}
+		if cp == 2 {
+			fmt.Println("\n" + user1 + " is a winner")
+		}
+		if cp == 3 {
+			fmt.Println("\n Game is a draw")
+		}
+
+	}
+
+	/*if ch1 == 1 && cp == 1 {
+		fmt.Println("\n" + user1 + " your choice is Rock")
 		fmt.Println("\n Game is a draw")
 	}
-	
-	if(ch1 == 1 && cp == 2) {
-		fmt.Println("\n User 2 is a winner")
+
+	if ch1 == 1 && cp == 2 {
+		fmt.Println("\n Computer is a winner")
 	}
 
-	if(ch1 == 1 && cp == 3) {
-		fmt.Println("\n User 1 is a winner")
+	if ch1 == 1 && cp == 3 {
+		fmt.Println("\n" + user1 + " is a winner")
 	}
 
-	if(ch1 == 2 && cp == 1) {
-		fmt.Println("\n User 1 is a winner")
+	if ch1 == 2 && cp == 1 {
+		fmt.Println("\n" + user1 + " is a winner")
 	}
-	
-	if(ch1 == 2 && cp == 2) {
+
+	if ch1 == 2 && cp == 2 {
 		fmt.Println("\n Game is a draw")
 	}
 
-	if(ch1 == 2 && cp == 3) {
-		fmt.Println("\n User 2 is a winner")
+	if ch1 == 2 && cp == 3 {
+		fmt.Println("\n Computer is a winner")
 	}
 
-	if(ch1 == 3 && cp == 1) {
-		fmt.Println("\n User 2 is a winner")
-	}
-	
-	if(ch1 == 3 && cp == 2) {
-		fmt.Println("\n User 1 is a winner")
+	if ch1 == 3 && cp == 1 {
+		fmt.Println("\n Computer is a winner")
 	}
 
-	if(ch1 == 3 && cp == 3) {
+	if ch1 == 3 && cp == 2 {
+		fmt.Println("\n" + user1 + " is a winner")
+	}
+
+	if ch1 == 3 && cp == 3 {
 		fmt.Println("\n Game is a draw")
-	}
+	}*/
 }
